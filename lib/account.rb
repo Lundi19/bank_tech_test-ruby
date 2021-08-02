aad2c3603c9c089675b6a65804d45d71563ff90c
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-08-02 13:44:43 +0200

    can initialize bank account with zero balance

---
+++ lib/account.rb
@@ -0,0 +1,8 @@
+class BankAccount
+  attr_reader :balance, :transactions
+
+  def initialize
+    @balance = 0
+    @transactions = transactions
+  end
+end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,1 +1,8 @@
 require 'account'
+
+describe BankAccount do
+  it 'can open a new account at $0' do
+    account = BankAccount.new
+    expect(account.balance).to eq(0)
+  end
+end
