8aaaa1e7117fd84a7ff13b9681caa027a48cc5f2
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-08-02 14:03:57 +0200

    refactor test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,13 +1,14 @@
 require 'account'
 
 describe BankAccount do
+
+  account = BankAccount.new
+  
   it 'can open a new account at $0' do
-    account = BankAccount.new
     expect(account.balance).to eq(0)
   end
 
   it 'can deposit money into the account' do
-    account = BankAccount.new
     account.deposit(10)
     expect(account.balance).to eq(10)
   end
