0d9d8ab8664ac6feb4de4e8a86a5024d912f01ad
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-08-02 17:56:54 +0200

    prints transactions refactor

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,11 +1,13 @@
-require_relative "account"
+require "account"
 
 class Statement
   def initialize(transactions)
     @transactions = transactions
+    @statement_trans = ""
+    transactions.each{ |item|  @statement_trans << item }
   end
 
   def print
-    "date || credit || debit || balance " + @transactions 
+    "date || credit || debit || balance #{@statement_trans}" 
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -8,7 +8,7 @@ describe Statement do
   describe do
     let(:account){ double("Account")}
     it "prints a statement with one transaction" do
-      allow(account).to receive(:transactions).and_return("\n 02/08/2021 || 210.00 || || 210.00" )
+      allow(account).to receive(:transactions).and_return(["\n 02/08/2021 || 210.00 || || 210.00"] )
   
       statement = Statement.new(account.transactions)
       expect(statement.print).to eq("date || credit || debit || balance \n 02/08/2021 || 210.00 || || 210.00")
