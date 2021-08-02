b9f60256f2afdaaa89ec269ad3de17a730c15fb9
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-08-02 17:36:49 +0200

    prints a statement with one transaction

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,5 +1,11 @@
 require_relative "account"
 
 class Statement
-  
+  def initialize(transactions)
+    @transactions = transactions
+  end
+
+  def print
+    "date || credit || debit || balance " + @transactions 
+  end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,8 +1,17 @@
 require 'statement'
 
 describe Statement do
-  it "should initialize a new statement" do
-    statement = Statement.new
-  end
+  # it "should initialize a new statement" do
+  #   statement = Statement.new
+  # end
 
+  describe do
+    let(:account){ double("Account")}
+    it "prints a statement with one transaction" do
+      allow(account).to receive(:transactions).and_return("\n 02/08/2021 || 210.00 || || 210.00" )
+  
+      statement = Statement.new(account.transactions)
+      expect(statement.print).to eq("date || credit || debit || balance \n 02/08/2021 || 210.00 || || 210.00")
+    end
+  end
 end
