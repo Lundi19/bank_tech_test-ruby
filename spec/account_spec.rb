0c3432f087ab94125977489a6259386d3d5aba4e
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-08-02 18:59:55 +0200

    test double for account and date

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,43 +3,36 @@ require 'date'
 
 describe BankAccount do
 
-
+  let(:account) { BankAccount.new }
+  let(:date) { Time.now.strftime('%d/%m/%Y') }
 
   it 'can open a new account at $0' do
-    account = BankAccount.new
     expect(account.balance).to eq(0)
   end
 
   it 'can deposit money into the account' do
-    account = BankAccount.new
     account.deposit(10)
     expect(account.balance).to eq(10)
   end
 
   it 'can withdraw money from the account' do
-    account = BankAccount.new
     account.deposit(200)
     account.withdraw(20)
     expect(account.balance).to eq(180)
   end
 
   it 'updates transaction history after depesoit including date' do
-    account = BankAccount.new
     account.deposit(200)
-    date = Time.now.strftime("%d/%m/%Y")
     expect(account.transactions).to include("\n#{date} || $200.00 || || $200.00")
   end
 
   it 'updates transaction history after withdrawl including date' do
-    account = BankAccount.new
     account.deposit(200)
     account.withdraw(100)
-    date = Time.now.strftime("%d/%m/%Y")
     expect(account.transactions).to include("\n#{date} || || $100.00 || $100.00")
   end
 
   it 'deposit fails if value is 0' do
-    account = BankAccount.new
     expect { account.deposit(0) }.to raise_error('You can only deposit an amount over 0')
   end
 end
