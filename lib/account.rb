b8e579385374e5bfbddd2ca466d834fb3c24f3ae
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-08-02 18:40:39 +0200

    refactor date into seperate method

--- lib/account.rb
+++ lib/account.rb
@@ -10,20 +10,22 @@ class BankAccount
   def deposit(amount)
     valid_trans?(amount)
     @balance += amount
-    date = Time.now.strftime("%d/%m/%Y")
-    @transactions << "\n#{date} || $#{amount}.00 || || $#{@balance}.00"
+    @transactions << "\n#{date_today} || $#{amount}.00 || || $#{@balance}.00"
   end
 
   def withdraw(amount)
     @balance -= amount
-    date = Time.now.strftime("%d/%m/%Y")
-    @transactions << "\n#{date} || || $#{amount}.00 || $#{@balance}.00"
+    @transactions << "\n#{date_today} || || $#{amount}.00 || $#{@balance}.00"
   end
 
   private
 
   def valid_trans?(amount)
-      raise 'You can only deposit an amount over 0' if amount <= 0
+    raise 'You can only deposit an amount over 0' if amount <= 0
+  end
+
+  def date_today
+    Time.now.strftime('%d/%m/%Y')
   end
 
 end
